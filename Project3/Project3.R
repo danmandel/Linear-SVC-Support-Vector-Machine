f35abd2a872c5b5ce0a3ffef0b6ef1a4f4f67ef8
danmandel <danmandel@users.noreply.github.com>
2015-02-27 22:43:26 -0500

    Update Project3.R

--- Project3/Project3.R
+++ Project3/Project3.R
@@ -1,10 +1,14 @@
 library(quantmod)
-getSymbols("AMZN")
+library(xts)
+library(xgboost)
+library(pROC)
 
-#barChart(AMZN, theme='white.mono',bar.type='hlc')
-barChart(AMZN)
+getSymbols("AMZN")
+#barChart(AMZN,theme='white.mono',bar.type='hlc') 
 
 getSymbols("^GSPC")
+chartSeries(GSPC, subset='last 3 months')
+addBBands(n = 20, sd = 2, ma = "SMA", draw = 'bands', on = -1)
 
 Nasdaq100_Symbols <- c("AAPL", "ADBE", "ADI", "ADP", "ADSK", "AKAM", "ALTR", "ALXN", 
                        "AMAT", "AMGN", "AMZN", "ATVI", "AVGO", "BBBY", "BIDU", "BIIB", 
@@ -21,6 +25,7 @@ Nasdaq100_Symbols <- c("AAPL", "ADBE", "ADI", "ADP", "ADSK", "AKAM", "ALTR", "AL
                        "VOD", "VRSK", "VRTX", "WDC", "WFM", "WYNN", "XLNX", "YHOO")
 getSymbols(Nasdaq100_Symbols)
 
+
 nasdaq100 <- data.frame(as.xts(merge(AAPL, ADBE, ADI, ADP, ADSK, AKAM, 
                                      ALTR, ALXN,AMAT, AMGN, AMZN, ATVI, AVGO, BBBY, BIDU, BIIB, 
                                      BRCM, CA, CELG, CERN, CHKP, CHRW, CHTR, CMCSA, 
@@ -34,15 +39,24 @@ nasdaq100 <- data.frame(as.xts(merge(AAPL, ADBE, ADI, ADP, ADSK, AKAM,
                                      ROST, SBAC, SBUX, SIAL, SIRI, SNDK, SPLS, SRCL, 
                                      STX, SYMC, TRIP, TSCO, TSLA, TXN, VIAB, VIP, 
                                      VOD, VRSK, VRTX, WDC, WFM, WYNN, XLNX, YHOO)))
+#head(nasdaq100[,1:12],2)
 
+# set outcome variable
 outcomeSymbol <- 'FISV.Volume'
-library(xts)
-#For training
+
+# shift outcome value to be on same line as predictors
 nasdaq100 <- xts(nasdaq100,order.by=as.Date(rownames(nasdaq100)))
 nasdaq100 <- as.data.frame(merge(nasdaq100, lm1=lag(nasdaq100[,outcomeSymbol],-1)))
 nasdaq100$outcome <- ifelse(nasdaq100[,paste0(outcomeSymbol,'.1')] > nasdaq100[,outcomeSymbol], 1, 0)
+
+# remove shifted down volume field 
 nasdaq100 <- nasdaq100[,!names(nasdaq100) %in% c(paste0(outcomeSymbol,'.1'))]
 
+# cast date to true date and order in decreasing order
+nasdaq100$date <- as.Date(row.names(nasdaq100))
+nasdaq100 <- nasdaq100[order(as.Date(nasdaq100$date, "%m/%d/%Y"), decreasing = TRUE),]
+
+# calculate all day differences and populate them on same row
 GetDiffDays <- function(objDF,days=c(10), offLimitsSymbols=c('outcome'), roundByScaler=3) {
   # needs to be sorted by date in decreasing order
   ind <- sapply(objDF, is.numeric)
@@ -60,7 +74,43 @@ GetDiffDays <- function(objDF,days=c(10), offLimitsSymbols=c('outcome'), roundBy
   return (objDF)
 }
 
+# call the function with the following differences
 nasdaq100 <- GetDiffDays(nasdaq100, days=c(1,2,3,4,5,10,20), offLimitsSymbols=c('outcome'), roundByScaler=2)
 
-#drop the most recent row because outcome is not yet known
+# drop most recent entry as we don't have an outcome
 nasdaq100 <- nasdaq100[2:nrow(nasdaq100),]
+
+# take a peek at YHOO features:
+#dput(names(nasdaq100)[grepl('YHOO.',names(nasdaq100))])
+
+# use POSIXlt to add day of the week, day of the month, day of the year
+nasdaq100$wday <- as.POSIXlt(nasdaq100$date)$wday
+nasdaq100$yday <- as.POSIXlt(nasdaq100$date)$mday
+nasdaq100$mon<- as.POSIXlt(nasdaq100$date)$mon
+
+# remove date field and shuffle data frame
+nasdaq100 <- subset(nasdaq100, select=-c(date))
+nasdaq100 <- nasdaq100[sample(nrow(nasdaq100)),]
+
+# model
+predictorNames <- names(nasdaq100)[names(nasdaq100) != 'outcome']
+
+set.seed(1234)
+split <- sample(nrow(nasdaq100), floor(0.7*nrow(nasdaq100)))
+train <-nasdaq100[split,]
+test <- nasdaq100[-split,]
+
+bst <- xgboost(data = as.matrix(train[,predictorNames]),
+               label = train$outcome,
+               verbose=0,
+               eta = 0.1,
+               gamma = 50, 
+               nround = 50,
+               colsample_bytree = 0.1,
+               subsample = 8.6,
+               objective="binary:logistic")
+
+predictions <- predict(bst, as.matrix(test[,predictorNames]), outputmargin=TRUE)
+
+auc <- roc(test$outcome, predictions)
+print(paste('AUC score:', auc$auc))
