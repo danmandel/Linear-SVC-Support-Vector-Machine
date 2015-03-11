a13085ee79498c21ca52f1af95a903309c461597
danmandel <danmandel@users.noreply.github.com>
2015-03-10 22:29:18 -0400

    Create Main.R

---
+++ MacroModel/Main.R
@@ -0,0 +1,22 @@
+library(quantmod)
+library(WDI)
+
+candleChart(GOOG,theme='black', type='candles')
+getSymbols("goog", src = "yahoo")
+getSymbols("DEXJPUS",src="FRED") # Japan / U.S. since 1971ish 
+#head(DEXJPUS)
+
+movavg <- avg(Cl(DEXJPUS))
+chartSeries(DEXJPUS)
+candleChart(DEXJPUS,subset='1940-12::2015')
+
+quandl
+
+#install.packages('WDI')
+
+#WDIsearch('gdp')[1:10,]
+WDI
+
+allPGDPdata = WDI(indicator='NY.GDP.PCAP.KD', country='all', start=1960, end=2016)
+WDIsearch('gdp.*capita.*constant')
+WDIsearch('gdp)
