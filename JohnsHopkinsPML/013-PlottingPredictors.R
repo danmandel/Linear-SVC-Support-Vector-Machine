f64523a7ffa6df26075f30785bde6352d8fe345c
danmandel <danmandel@users.noreply.github.com>
2015-04-01 23:02:29 -0400

    Update 013-PlottingPredictors.R

--- JohnsHopkinsPML/013-PlottingPredictors.R
+++ JohnsHopkinsPML/013-PlottingPredictors.R
@@ -1,4 +1,4 @@
-library(ISLR);library(ggplot2);library(caret);
+library(ISLR);library(ggplot2);library(caret);library(Hmisc);
 data(Wage)
 summary(Wage)
 
@@ -13,4 +13,26 @@ featurePlot(x=trainingSet[,c("age","education","jobclass")],
             plot="pairs")
 
 qplot(age,wage,color=jobclass,data=trainingSet)
+
+qq <- qplot(age,wage,color=education,data=trainingSet)
+qq +geom_smooth(method='lm',formula=y~x)
+
+cutWage <- cut2(trainingSet$wage,g=3)
+table(cutWage)
+
+p1 <- qplot(cutWage,age,data=trainingSet,fill=cutWage,
+            geom=c("boxplot"))
+p1
+
+p2 <- qplot(cutWage,age,data=trainingSet,fill=cutWage,
+            geom=c("boxplot","jitter"))
+p2
+
+t1 <- table(cutWage,trainingSet$jobclass)
+t1
+
+prop.table(t1,1)
+qplot(wage,color=education,data=trainingSet,geom="density")
+
+
 
