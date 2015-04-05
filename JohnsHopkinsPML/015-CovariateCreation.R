cc5c362dd8471de765cd40aab8e12a9df7d0679c
danmandel <danmandel@users.noreply.github.com>
2015-04-04 21:50:11 -0400

    Update 015-CovariateCreation.R

--- JohnsHopkinsPML/015-CovariateCreation.R
+++ JohnsHopkinsPML/015-CovariateCreation.R
@@ -1,1 +1,31 @@
+library(kernlab);data(spam)
+
+#transforming tidy covariates
+spam$capitalAvesq <- spam$capitalAve^2
+
+#dummy variables
+library(ISLR);library(caret);data(Wage);
+inTrainingSet <- createDataPartition(y=Wage$wage,
+                                     p=.7,list=FALSE)
+trainingSet <- Wage[inTrainingSet,]; testingSet <- Wage[-inTrainingSet,]
+table(trainingSet$jobclass)
+dummies <- dummyVars(wage ~ jobclass, data=trainingSet)
+head(predict(dummies,newdata=trainingSet))
+
+#removing zero covariates
+nsv <- nearZeroVar(trainingSet,saveMetrics=TRUE)
+nsv
+
+#spline basis
+library(splines)
+bsBasis <- bs(trainingSet$age,df=3)
+bsBasis
+
+#fitting curves with splines
+lml <- lm(wage ~ bsBasis,data=trainingSet)
+plot(trainingSet$age,trainingSet$wage,pch=19,cex=.5)
+points(trainingSet$age,predict(lml,newdata=trainingSet),col="red",pch=19,cex=.5)
+
+#splines on test set
+predict(bsBasis,age=testingSet$age)
 
