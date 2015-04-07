82ff7eb76b1546b871c161aa751c54c3f53aae54
danmandel <danmandel@users.noreply.github.com>
2015-04-07 17:52:56 -0400

    Update 016-PreProcessing2.R

--- JohnsHopkinsPML/016-PreProcessing2.R
+++ JohnsHopkinsPML/016-PreProcessing2.R
@@ -8,3 +8,15 @@ testingSet <- spam[-inTrainingSet,]
 M <- abs(cor(trainingSet[,-58]))
 diag(M) <- 0
 which(M > .8,arr.ind=T)
+
+names(spam)[c(34,32)]
+plot(spam[,34],spam[,32])
+
+#rotate the plot
+X <- .71*trainingSet$num415 + .71*trainingSet$num857
+Y <- .71*trainingSet$num415 - .71*trainingSet$num857
+plot(X,Y)
+
+smallSpam <- spam[,c(34,32)]
+prComp <- prcomp(smallSpam)
+plot(prComp$x[,1],prComp$x[,2])
