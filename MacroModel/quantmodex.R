05f75c0121553b9245b707d3da69f6af44c7d74a
danmandel <danmandel@users.noreply.github.com>
2015-04-08 23:16:21 -0400

    Update quantmodex.R

--- MacroModel/quantmodex.R
+++ MacroModel/quantmodex.R
@@ -1,10 +1,10 @@
 library(quantmod)
-library(YieldCurve)
 library(caret)
+library(e1071)
 #getSymbols('SPY',src = 'yahoo')
 getSymbols('^VIX',src='yahoo')
 getFX('EUR/USD',src = 'yahoo')
-getSymbols('CPIAUCSL',src='FRED')
+#getSymbols('CPIAUCSL',src='FRED')
 getSymbols('DGS10',src='FRED')
 
 dataSPY = SPY['2014::']
@@ -12,42 +12,35 @@ dataVIX = VIX['2015::']
 todayclose = EURUSD['2015::']
 prevclose <- lag(todayclose,1) # now the value for jan 2 is the price it was on jan 1
 nextclose <- lag(todayclose,-1)
-check <- merge(prevclose,todayclose,nextclose,nextday,nextday3)
-colnames(check) = c("prevclose","close","nextclose","nextday","nextday3")
-
-nextday = ifelse(nextclose>=todayclose,1,-1)
-nextday3 = ifelse(nextclose>todayclose,1,ifelse(nextclose<todayclose,-1,0))
-
-nextday3 = {
- if (nextclose[i]>todayclose[i]){
-   nextday2[i] <- 1
- } else if (nextclose[i]==todayclose[i]){
-   nextday2[i] <- 0
- }
-  else if (nextclose[i]<todayclose[i]){
-    nextday2[i] <- -1
-  }
-}
+nextday = ifelse(nextclose>todayclose,1,ifelse(nextclose<todayclose,-1,0))
+dataset <- merge(prevclose,todayclose,nextclose,nextday,DGS10['2015::'])
+colnames(dataset) = c("prevclose","close","nextclose","nextday","DGS110")
 
 
-total <- merge(DGS10,EURUSD,VIX)
-curonly <- EURUSD['2015::']
-nextday = ifelse 
-total2015 <- total['2015::']
 
-index = 1:nrow(total2015)
+index = 1:nrow(dataset)
 inTrainingSet = createDataPartition(index,p=.75,
                                     list=FALSE)
-training_orig = total2015[inTrainingSet,]
-testing_orig = total2015[-inTrainingSet,]
+training_orig = dataset[inTrainingSet,]
+testing_orig = dataset[-inTrainingSet,]
 
-training = as.data.frame(total2015[inTrainingSet,])
+training = as.data.frame(dataset[inTrainingSet,])
 rownames(training) = NULL
+training$nextday = as.factor(training$nextday)
 
+testing = as.data.frame(dataset[-inTrainingSet,])
+rownames(testing) = NULL
+testing$nextday = as.factor(testing$nextday)
 
+svm.model <- svm(nextday~.,data=training,cost=10^1,gamma=10^-2)
+svm.pred = predict(svm.model,testing[,-ncol(testing)])
 
 #mm <- specifyModel(Next(OpCl(dataSPY)) ~ OpCl(dataSPY) + Cl(dataVIX))
 #modelData(mm)
 #candleChart(SPY,theme='white.mono', type='candles') 
 #addSMI(n=13,slow=25,fast=2,signal=9,ma.type="EMA")
 #buildData(formula, na.rm = TRUE, return.class = "zoo")
+
+
+
+  
