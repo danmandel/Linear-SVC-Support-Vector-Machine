da7be6f6a2a112ade5d884ddaafb117ff08b2f65
danmandel <danmandel@users.noreply.github.com>
2015-04-12 13:17:56 -0400

    Update 016-PreProcessing2.R

--- JohnsHopkinsPML/016-PreProcessing2.R
+++ JohnsHopkinsPML/016-PreProcessing2.R
@@ -27,3 +27,6 @@ typeColor <- ((spam$type=="spam")*1+1)
 prComp <- prcomp(log10(spam[,-58]+1))
 plot(prComp$x[,1],prComp$x[,2],col=typeColor,xlab="PC1",ylab="PC2")
 
+preProc <- preProcess(log10(spam[,-58]+1),method="pca",pcaComp=2)
+spamPC <- predict(preProc,log10(spam[,-58]+1))
+plot(spamPC[,1],spamPC[,2],col=typeColor)
